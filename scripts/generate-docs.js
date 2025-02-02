99676790af0e65959c0be6707751e3da388e3602
Paul Joseph <pajoseph@guidewire.com>
2025-02-02 14:12:47 +0530

    feat: update docs folder structure
    Signed-off-by: Paul Joseph <pajoseph@guidewire.com>

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -8,7 +8,7 @@ async function generateDocs() {
         // Read template and markdown
         const [templateContent, markdown] = await Promise.all([
             fs.readFile(path.join(__dirname, '../_templates/doc-page.html'), 'utf8'),
-            fs.readFile(path.join(__dirname, '../content/README.md'), 'utf8')
+            fs.readFile(path.join(__dirname, '../docs/README.md'), 'utf8')
         ]);
 
         // Convert markdown to HTML
